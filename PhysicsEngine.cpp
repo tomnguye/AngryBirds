e92970f47d1b7124a84e2499074433703fab4aa3
Ryan <u6913762@anu.edu.au>
2026-05-03 22:52:39 +1000

    Update physics engine code to be more readable

--- PhysicsEngine.cpp
+++ PhysicsEngine.cpp
@@ -25,98 +25,101 @@ void PhysicsEngine::advanceState() {
         b->sleeping = false;
     }
 
-    struct ContactImpulse { float normal = 0.f; float friction = 0.f; };
+    struct ContactImpulseAccumulator { float normal = 0.f; float friction = 0.f; };
     int totalContacts = 0;
-    for (auto& e : c) totalContacts += e.contactPoints.size();
-    std::vector<ContactImpulse> impulses(totalContacts);
+    for (auto& event : c) totalContacts += event.contactPoints.size();
+    std::vector<ContactImpulseAccumulator> accumulatedImpulses(totalContacts);
 
-    // Compute target velocities before iteration loop
-    struct ContactTarget { float targetVel = 0.f; };
-    std::vector<ContactTarget> targets(totalContacts);
+    struct preImpulseData { float targetVelocity = 0.f; };
+    std::vector<preImpulseData> preSolveData(totalContacts);
+
     {
         int ci = 0;
         for (auto& event : c) {
             PhysicsObject* a = event.objA;
             PhysicsObject* b = event.objB;
-            Eigen::Vector2f normal = event.contactNormal;
+            Eigen::Vector2f n = event.contactNormal;
+
             for (int i = 0; i < (int)event.contactPoints.size(); i++, ci++) {
-                Eigen::Vector2f point = event.contactPoints[i];
-                Eigen::Vector2f ra = point - a->position;
-                Eigen::Vector2f rb = point - b->position;
-                Eigen::Vector2f raPerp = {-a->angularVelocity * ra.y(), a->angularVelocity * ra.x()};
-                Eigen::Vector2f rbPerp = {-b->angularVelocity * rb.y(), b->angularVelocity * rb.x()};
-                Eigen::Vector2f relVel = (b->velocity + rbPerp) - (a->velocity + raPerp);
-                float van = relVel.dot(normal);
-                float e = (fabsf(van) < 0.2f) ? 0.0f : restitution;
-                targets[ci].targetVel = -e * van;
+                Eigen::Vector2f contactPoint = event.contactPoints[i];
+                Eigen::Vector2f rA = contactPoint - a->position;
+                Eigen::Vector2f rB = contactPoint - b->position;
+
+                Eigen::Vector2f vA = a->velocity + Eigen::Vector2f{-a->angularVelocity * rA.y(), a->angularVelocity * rA.x()};
+                Eigen::Vector2f vB = b->velocity + Eigen::Vector2f{-b->angularVelocity * rB.y(), b->angularVelocity * rB.x()};
+                Eigen::Vector2f vRel = vB - vA;
+
+                float vn = vRel.dot(n);
+                float e = (fabsf(vn) < 0.2f) ? 0.0f : restitution;
+                preSolveData[ci].targetVelocity = -e * vn;
             }
         }
     }
 
-    for (int j = 0; j < maxIterations; j++) {
+    for (int iteration = 0; iteration < maxIterations; iteration++) {
         int ci = 0;
         for (auto& event : c) {
             PhysicsObject* a = event.objA;
             PhysicsObject* b = event.objB;
-            Eigen::Vector2f normal = event.contactNormal;
+            Eigen::Vector2f n = event.contactNormal;
 
             for (int i = 0; i < (int)event.contactPoints.size(); i++, ci++) {
-                Eigen::Vector2f point = event.contactPoints[i];
-                Eigen::Vector2f ra = point - a->position;
-                Eigen::Vector2f rb = point - b->position;
+                Eigen::Vector2f contactPoint = event.contactPoints[i];
+                Eigen::Vector2f rA = contactPoint - a->position;
+                Eigen::Vector2f rB = contactPoint - b->position;
 
-                Eigen::Vector2f raPerp = {-a->angularVelocity * ra.y(), a->angularVelocity * ra.x()};
-                Eigen::Vector2f rbPerp = {-b->angularVelocity * rb.y(), b->angularVelocity * rb.x()};
-                Eigen::Vector2f relativeVelocity = (b->velocity + rbPerp) - (a->velocity + raPerp);
+                Eigen::Vector2f vA = a->velocity + Eigen::Vector2f{-a->angularVelocity * rA.y(), a->angularVelocity * rA.x()};
+                Eigen::Vector2f vB = b->velocity + Eigen::Vector2f{-b->angularVelocity * rB.y(), b->angularVelocity * rB.x()};
+                Eigen::Vector2f vRel = vB - vA;
 
-                float velAlongNormal = relativeVelocity.dot(normal);
+                float vn = vRel.dot(n);
 
-                float raCrossN = ra.x() * normal.y() - ra.y() * normal.x();
-                float rbCrossN = rb.x() * normal.y() - rb.y() * normal.x();
-                float effMass = a->inv_mass + b->inv_mass
-                    + raCrossN * raCrossN * a->inv_inertia
-                    + rbCrossN * rbCrossN * b->inv_inertia;
+                float rACrossN = rA.x() * n.y() - rA.y() * n.x();
+                float rBCrossN = rB.x() * n.y() - rB.y() * n.x();
+                float kN = a->inv_mass + b->inv_mass
+                    + rACrossN * rACrossN * a->inv_inertia
+                    + rBCrossN * rBCrossN * b->inv_inertia;
 
-                float jRaw = (-velAlongNormal + targets[ci].targetVel) / effMass;
+                float jn = (-vn + preSolveData[ci].targetVelocity) / kN;
 
-                float oldNormal = impulses[ci].normal;
-                impulses[ci].normal = std::max(oldNormal + jRaw, 0.0f);
-                float jDelta = impulses[ci].normal - oldNormal;
+                float jnOld = accumulatedImpulses[ci].normal;
+                accumulatedImpulses[ci].normal = std::max(jnOld + jn, 0.0f);
+                float jnDelta = accumulatedImpulses[ci].normal - jnOld;
 
-                Eigen::Vector2f impulse = jDelta * normal;
-                a->velocity -= impulse * a->inv_mass;
-                b->velocity += impulse * b->inv_mass;
-                a->angularVelocity -= (ra.x() * impulse.y() - ra.y() * impulse.x()) * a->inv_inertia;
-                b->angularVelocity += (rb.x() * impulse.y() - rb.y() * impulse.x()) * b->inv_inertia;
+                Eigen::Vector2f Jn = jnDelta * n;
+                a->velocity -= Jn * a->inv_mass;
+                b->velocity += Jn * b->inv_mass;
+                a->angularVelocity -= (rA.x() * Jn.y() - rA.y() * Jn.x()) * a->inv_inertia;
+                b->angularVelocity += (rB.x() * Jn.y() - rB.y() * Jn.x()) * b->inv_inertia;
 
-                Eigen::Vector2f raPerp2 = {-a->angularVelocity * ra.y(), a->angularVelocity * ra.x()};
-                Eigen::Vector2f rbPerp2 = {-b->angularVelocity * rb.y(), b->angularVelocity * rb.x()};
-                Eigen::Vector2f relVelAfter = (b->velocity + rbPerp2) - (a->velocity + raPerp2);
+                // Recompute relative velocity after normal impulse for friction
+                Eigen::Vector2f vA2 = a->velocity + Eigen::Vector2f{-a->angularVelocity * rA.y(), a->angularVelocity * rA.x()};
+                Eigen::Vector2f vB2 = b->velocity + Eigen::Vector2f{-b->angularVelocity * rB.y(), b->angularVelocity * rB.x()};
+                Eigen::Vector2f vRel2 = vB2 - vA2;
 
-                Eigen::Vector2f tangent = relVelAfter - relVelAfter.dot(normal) * normal;
-                float tangentLen = tangent.norm();
-                if (tangentLen < 1e-6f) continue;
-                tangent /= tangentLen;
+                Eigen::Vector2f vt = vRel2 - vRel2.dot(n) * n;
+                float vtLen = vt.norm();
+                if (vtLen < 1e-6f) continue;
+                Eigen::Vector2f t = vt / vtLen;
 
-                float raCrossT = ra.x() * tangent.y() - ra.y() * tangent.x();
-                float rbCrossT = rb.x() * tangent.y() - rb.y() * tangent.x();
-                float kTangent = a->inv_mass + b->inv_mass
-                    + raCrossT * raCrossT * a->inv_inertia
-                    + rbCrossT * rbCrossT * b->inv_inertia;
+                float rACrossT = rA.x() * t.y() - rA.y() * t.x();
+                float rBCrossT = rB.x() * t.y() - rB.y() * t.x();
+                float kT = a->inv_mass + b->inv_mass
+                    + rACrossT * rACrossT * a->inv_inertia
+                    + rBCrossT * rBCrossT * b->inv_inertia;
 
-                float vt = relVelAfter.dot(tangent);
-                float jtRaw = -vt / kTangent;
+                float jt = -vRel2.dot(t) / kT;
 
-                float maxFriction = 0.3f * impulses[ci].normal;
-                float oldFriction = impulses[ci].friction;
-                impulses[ci].friction = std::clamp(oldFriction + jtRaw, -maxFriction, maxFriction);
-                float jtDelta = impulses[ci].friction - oldFriction;
+                float coulombLimit = 0.3f * accumulatedImpulses[ci].normal;
+                float jtOld = accumulatedImpulses[ci].friction;
+                accumulatedImpulses[ci].friction = std::clamp(jtOld + jt, -coulombLimit, coulombLimit);
+                float jtDelta = accumulatedImpulses[ci].friction - jtOld;
 
-                Eigen::Vector2f frictionImpulse = jtDelta * tangent;
-                a->velocity -= frictionImpulse * a->inv_mass;
-                b->velocity += frictionImpulse * b->inv_mass;
-                a->angularVelocity -= (ra.x() * frictionImpulse.y() - ra.y() * frictionImpulse.x()) * a->inv_inertia;
-                b->angularVelocity += (rb.x() * frictionImpulse.y() - rb.y() * frictionImpulse.x()) * b->inv_inertia;
+                Eigen::Vector2f Jf = jtDelta * t;
+                a->velocity -= Jf * a->inv_mass;
+                b->velocity += Jf * b->inv_mass;
+                a->angularVelocity -= (rA.x() * Jf.y() - rA.y() * Jf.x()) * a->inv_inertia;
+                b->angularVelocity += (rB.x() * Jf.y() - rB.y() * Jf.x()) * b->inv_inertia;
             }
         }
     }
@@ -187,9 +190,4 @@ void PhysicsEngine::updatePhysics(PhysicsObject& obj)
 
     obj.position += obj.velocity * dt;
     obj.rotation += obj.angularVelocity * dt;
-
-
-    
-    
-
 }
